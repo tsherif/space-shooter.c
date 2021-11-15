251f662785f1e91344cb20e0b17ce9adab3c8932
Tarek Sherif <tareksherif@pm.me>
2021-11-14 20:33:34 -0500

    Windows debug vs release builds

--- build.bat
+++ build.bat
@@ -1,8 +1,19 @@
+@if "%~1" == "release" (
+	set arg1=release
+) else (
+	set arg1=debug
+)
+@set shared_args=/std:c11 /W3 /WX /wd5105 /D SOGL_MAJOR_VERSION=4 /D SOGL_MINOR_VERSION=5 /Fespace-shooter
+@if %arg1%==release (
+	@set build_args=/O2
+) else (
+	@set build_args=/Zi
+)
 @rmdir /s /q build
 @mkdir build
 @xcopy /S /y assets build\assets\
 @pushd build
 @REM /wd5105 suppressed because it causes an error in winbase.h
-cl /std:c11 /Zi /W3 /WX /wd5105 /D SOGL_MAJOR_VERSION=4 /D SOGL_MINOR_VERSION=5 /Fespace-shooter ..\src\shared\*.c ..\src\game\*.c ..\src\platform\windows\*.c user32.lib gdi32.lib opengl32.lib xinput.lib ole32.lib
+cl %shared_args% %build_args% ..\src\shared\*.c ..\src\game\*.c ..\src\platform\windows\*.c user32.lib gdi32.lib opengl32.lib xinput.lib ole32.lib
 @popd
  
